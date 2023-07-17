93232c8fa7f6e3d64f167d14a24c93ce1e93b175
Satoko <satoko@suzuki.email.ne.jp>
2023-07-17 23:19:35 +0900

    不要パッケージ削除

--- config-overrides.js
+++
@@ -1,6 +0,0 @@
-const { override, addWebpackPlugin } = require('customize-cra');
-const { VanillaExtractPlugin } = require('@vanilla-extract/webpack-plugin');
-
-module.exports = override(
-    addWebpackPlugin(new VanillaExtractPlugin())
-);
